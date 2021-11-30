c2c670814db6a19262dfe73a20628a5d3974513c
iammustafatz <iammustafatz26@gmail.com>
2021-11-30 16:51:49 +0530

    f

--- components/Order.js
+++ components/Order.js
@@ -23,7 +23,7 @@ function Order() {
     <div className="lg:text-left">
     <div className="max-w-md rounded-lg container" ref={animcontainer}></div> 
     </div> 
-    <div className="card xs:-p-4 xs:-m-2 flex-shrink w-full max-w-sm shadow-2xl bg-base-100">
+    <div className="card flex-shrink w-full max-w-sm shadow-2xl bg-base-100">
       <div className="card-body">
       <form className="mt-2 space-y-6" action="#" method="POST">
       <div className="grid grid-cols-6 gap-6">
